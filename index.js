1f5da6e60e455447a2bdb729af7f1530b924a911
Vivek Namdev <viveknamdev257@gmail.com>
2024-10-13 23:31:37 +0530

    added user signup logic

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
 const {adminRouter} = require("./routes/admin");
 const app = express();
+app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,33 @@
 const {Router} = require("express");
+const {userModel} = require("../db");
+const bcrypt = require("bcrypt");
+const zod = require("zod");
 
 const userRouter = Router();  
 
-  userRouter.post("/signup", function(req, res) {
-        res.json({
-            message: "signup endpoint"
-        })
+  userRouter.post("/signup", async function(req, res) {
+    const {email, password, firstName, lastName} = req.body;
+
+try {
+   await userModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
     })
+        res.json({
+            message: "signup succeeded"
+        })
+
+    } 
+
+catch(error) {
+    res.status(500).json({
+        message: "Signup failed",
+        error: error.message
+    })
+}
+})
     
     userRouter.post("/signin", function(req, res) {
         res.json({
